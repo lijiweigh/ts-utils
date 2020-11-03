7745a3a9eece7541f3f10a87ec03b23a90cf3003
lijiwei <lijiwei@fenbi.com>
2020-11-03 11:33:35 +0800

    ...

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,15 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/test/index.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/test/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="test">
+        <iframe width="1140" height="500" src="http://shimo.zhenguanyu.com/desktop"></iframe>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+
+@Component 
+export default class Test extends Vue {
+
+}
+</script>
